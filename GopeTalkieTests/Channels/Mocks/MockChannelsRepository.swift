dbb733c8fe1a974f555c639169408304f0b34d7f
Carlos Mantilla <carlosmcot@gmail.com>
2025-08-27 07:24:13 -0500

    Test: Change property names to match all mock of the repositories

--- GopeTalkieTests/Channels/Mocks/MockChannelsRepository.swift
+++ GopeTalkieTests/Channels/Mocks/MockChannelsRepository.swift
@@ -9,10 +9,10 @@ import XCTest
 @testable import GopeTalkie
 
 final class MockChannelsRepository: ChannelsRepositoryProtocol {
-    var stubbedResult: Result<[Channel], Error>?
+    var resultToReturn: Result<[Channel], Error>?
 
     func fetchChannels(completion: @escaping (Result<[Channel], Error>) -> Void) {
-        if let result = stubbedResult {
+        if let result = resultToReturn {
             completion(result)
         }
     }
